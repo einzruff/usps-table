c1c291af4e336592fba1cdf79e166d133d713b33
Daphne Lundquist <lundquja@gmail.com>
2018-08-02 16:53:19 -0700

    initial commit

---
+++ usps-table/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace usps_table
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lvResults = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(12, 12);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(516, 20);
+            this.txtURL.TabIndex = 0;
+            this.txtURL.Text = "https://pe.usps.com/text/dmm300/Notice123.htm";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(628, 9);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "button1";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 38);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(614, 221);
+            this.txtResult.TabIndex = 2;
+            // 
+            // lvResults
+            // 
+            this.lvResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lvResults.Location = new System.Drawing.Point(12, 265);
+            this.lvResults.Name = "lvResults";
+            this.lvResults.Size = new System.Drawing.Size(614, 148);
+            this.lvResults.TabIndex = 3;
+            this.lvResults.UseCompatibleStateImageBehavior = false;
+            this.lvResults.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 255;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 708);
+            this.Controls.Add(this.lvResults);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.ListView lvResults;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ usps-table/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace usps_table
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string urlAddress = txtURL.Text;
+            string datafound = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+                datafound = readStream.ReadToEnd();
+                response.Close();
+                readStream.Close();
+            }
+            string letter1oz = "";
+            string letter2oz = "";
+            string letter3oz = "";
+            string letter35oz = "";
+            string large1oz = "";
+            string large2oz = "";
+            string large3oz = "";
+            string large4oz = "";
+            string large5oz = "";
+            string large6oz = "";
+            string large7oz = "";
+            string large8oz = "";
+            string large9oz = "";
+            string large10oz = "";
+            string large11oz = "";
+            string large12oz = "";
+            string large13oz = "";
+            //US!
+            //_c037 contains  Letters (Stamped)  Large Envelopes   Postcard
+            //_c038 is the letters
+            //_c107 containts  First-Class Package (Parcels) rates
+            //CANADA!
+            //_c341 contains  Canada First-Class  Postcards, Letters, Large Envelopes
+            //<div id="_c341">
+            //_c342 is just the postcards
+            //_c343 is the letters
+            //_c348 is the large envelopes
+            ///////////////////////////////////////////////////////////////////////////////////////////////
+            //Canada Postcard
+            string canada = "<div id=\"_c342\">";
+            int found342 = datafound.IndexOf(canada);
+            //start at found341 and get the first <tbody>
+            int tbody342 = datafound.IndexOf("<tbody>", found342);
+            //start at tbody342 amd get the 2nd <td>
+            int td342first = datafound.IndexOf("<td>", tbody342);
+            int td342second = datafound.IndexOf("<td>", td342first+5);
+            //get the position of the end </td>
+            int td342endtd = datafound.IndexOf("</td>",td342second);
+            //get string of the Canada postcard amount
+            string td342outerstr = datafound.Substring(td342second, (td342endtd - td342second));
+            int td342amt = td342outerstr.IndexOf("$");
+            int td342amtend = td342outerstr.IndexOf("</p>");
+            string canadapostcard = td342outerstr.Substring(td342amt, (td342amtend - td342amt));
+            if (found342 != -1)
+            {
+                lvResults.Items.Add("Canada Postcard: " + canadapostcard);
+            }
+            ///////////////////////////////////////////////////////////////////////////////////////////////
+            //
+        }
+    }
+}
